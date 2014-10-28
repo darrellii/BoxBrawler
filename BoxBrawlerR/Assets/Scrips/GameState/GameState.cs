7bf53bc27e4a19a58114ee4b0b427fecc7b52c78
DJ Todd <dtodd67@gmail.com>
2014-10-28 11:00:29 -0500

    init commit

---
+++ BoxBrawlerR/Assets/Scrips/GameState/GameState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	PlayerValues player1;
+	PlayerValues player2;
+	PlayerValues player3;
+	PlayerValues player4;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+}
---
+++ BoxBrawlerR/Assets/Scrips/Network/NetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "RoomName";
+	
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+	}
+
+}
